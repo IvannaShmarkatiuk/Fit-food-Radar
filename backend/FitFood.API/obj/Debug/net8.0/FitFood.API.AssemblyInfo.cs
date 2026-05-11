515bd72db58f01ddfb69ac3ebc8d7402f19774d6
Maya Kravets <maia.kravets@gmail.com>
2026-05-11 22:38:13 +0300

    bags fixed

--- backend/FitFood.API/obj/Debug/net8.0/FitFood.API.AssemblyInfo.cs
+++ backend/FitFood.API/obj/Debug/net8.0/FitFood.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitFood.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b8bed53228413365e5d9150a298050f0179d75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f8e8e1f48005711f7cdfc204290922d7f110dd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitFood.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitFood.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
