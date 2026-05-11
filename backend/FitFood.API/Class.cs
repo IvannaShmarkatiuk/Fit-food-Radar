77b8bed53228413365e5d9150a298050f0179d75
Maya Kravets <maia.kravets@gmail.com>
2026-05-11 18:19:46 +0300

    class

---
+++ backend/FitFood.API/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace FitFood.API
+{
+    public class Class
+    {
+    }
+}
--- backend/FitFood.API/obj/Debug/net8.0/FitFood.API.AssemblyInfo.cs
+++ backend/FitFood.API/obj/Debug/net8.0/FitFood.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitFood.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce6a9a570acdd7aad38f9ef311a13ccb1e316d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ecad74a67783b0dff182dcb102643e674332db")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitFood.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitFood.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
